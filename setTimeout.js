fbd62e3e619cecea1abc8c57f1fb7aba4603a764
Sunil <ssunilkumar787@gmail.com>
2024-08-31 20:30:09 +0530

    setTimeout

---
+++ setTimeout.js
@@ -0,0 +1,21 @@
+// const x = () => {
+//    for(let i=1; i<=5; i++) {
+//     setTimeout(() => {
+//         console.log(i)
+//     },i * 1000)
+//    }
+// }
+// x()
+
+// const x = () => {
+//   for (var i = 1; i <= 5; i++) {
+//     const close = (i) => {
+//       setTimeout(() => {
+//         console.log(i);
+//       }, i * 1000);
+//     };
+//     close(i)
+//   }
+  
+// };
+// x()
