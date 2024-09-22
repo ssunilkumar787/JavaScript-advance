00233322abb47bb0b8e41e9c4566c4e0165690b4
Sunil <ssunilkumar787@gmail.com>
2024-09-22 20:06:28 +0530

    splice

--- setTimeout.js
+++ setTimeout.js
@@ -19,3 +19,19 @@
   
 // };
 // x()
+
+console.log('start')
+
+setTimeout(() => {
+    console.log('callback')
+},0)
+
+console.log('end')
+
+// let startDate = new Date().getTime()
+// let endDate = startDate;
+
+// while(endDate < startDate + 10000) {
+//     endDate = new Date().getTime()
+// }
+// console.log('while expires')
---
+++ splice.js
@@ -0,0 +1,5 @@
+const arr = [1,2,3,4,5,6]
+
+const splicedArr = arr.splice(9,1, 8,9)
+
+console.log(splicedArr,arr)
